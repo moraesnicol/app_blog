b34d1a2fd6319bfe758940f4ed6cbe3d8c4be958
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-08 02:46:47 -0300

    add_blog_page added

---
+++ lib/pages/add_blog_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddBlog extends StatefulWidget {
+
+  
+  @override
+  _AddBlogState createState() => _AddBlogState();
+}
+
+class _AddBlogState extends State<AddBlog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
@@ -29,12 +30,19 @@ class _HomePageState extends State<HomePage> {
           physics: BouncingScrollPhysics(),
           children: [
             SingleItem(),
+            SingleItem(),
+            SingleItem(),
+            SingleItem(),
+            SingleItem(),
+            SingleItem(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+        child: Icon(Icons.post_add_rounded),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddBlogPage()  ));
+        },
       ),
     );
   }
@@ -44,30 +52,77 @@ class SingleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+          color: Color(0xFF101010), borderRadius: BorderRadius.circular(5)),
       margin: EdgeInsets.all(15),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image(
-            width: MediaQuery.of(context).size.width / 1.1,
-            height: MediaQuery.of(context).size.height / 3.0,
-            image:
-                NetworkImage('https://randomuser.me/api/portraits/men/73.jpg'),
-            fit: BoxFit.cover,
-            // filterQuality: FilterQuality.medium,
-          ),
+          Stack(alignment: Alignment.bottomCenter, children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image(
+                width: MediaQuery.of(context).size.width / 1.1,
+                height: MediaQuery.of(context).size.height / 3.0,
+                image: NetworkImage(
+                    'https://randomuser.me/api/portraits/men/73.jpg'),
+                fit: BoxFit.cover,
+                // filterQuality: FilterQuality.medium,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 45,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black87],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          ]),
           SizedBox(height: 10),
-          Text('Título do post',
-              style: GoogleFonts.roboto(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold)),
-          SizedBox(height: 10),
-          Text('Título do post',
-              style: GoogleFonts.roboto(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold)),
-                  
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Título do post',
+                      style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: 5),
+                  Text('Descrição do post',
+                      style: GoogleFonts.roboto(
+                          color: Colors.blue,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold)),
+                ],
+              )),
+          Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite_border_rounded,
+                  size: 30,
+                  color: Colors.yellow[300],
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.comment,
+                  size: 25,
+                  color: Colors.yellow[300],
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.shareAlt,
+                  size: 25,
+                  color: Colors.yellow[300],
+                )),
+          ]),
+          SizedBox(height: 10)
         ],
       ),
     );
