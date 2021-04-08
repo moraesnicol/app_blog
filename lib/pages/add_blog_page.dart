e60af78082c4a4f6c87f8ea00b9fdaeffb754782
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-08 14:46:33 -0300

    image_picker added

--- lib/pages/add_blog_page.dart
+++ lib/pages/add_blog_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddBlogPage extends StatefulWidget {
   @override
@@ -7,10 +10,25 @@ class AddBlogPage extends StatefulWidget {
 }
 
 class _AddBlogState extends State<AddBlogPage> {
-  
   TextEditingController _titleController = TextEditingController();
   TextEditingController _descriptionController = TextEditingController();
 
+  bool _isLoading = false;
+
+  File _image;
+  final picker = ImagePicker();
+
+  // ignore: unused_element
+  Future _getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +38,10 @@ class _AddBlogState extends State<AddBlogPage> {
         backgroundColor: Colors.amber,
         elevation: 0.3,
         brightness: Brightness.dark,
-        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, size: 30, color: Colors.red),
+          onPressed: () => Navigator.pop(context),
+        ),
         title: Text(
           'EBT blog',
           style: GoogleFonts.roboto(
@@ -28,19 +49,12 @@ class _AddBlogState extends State<AddBlogPage> {
         ),
         actions: [
           Container(
-            width: 120,
-            height: 20,
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: TextButton(
-              onPressed: () {},
-              child: Text('Upload', style: GoogleFonts.roboto()),
-              style: TextButton.styleFrom(
-                  primary: Colors.teal,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15))),
-                  backgroundColor: Colors.red),
-            ),
-          ),
+              width: 120,
+              height: 20,
+              child: IconButton(
+                onPressed: () => _getImage(),
+                icon: Icon(Icons.add, size: 30, color: Colors.red),
+              )),
         ],
       ),
       body: Container(
@@ -48,32 +62,34 @@ class _AddBlogState extends State<AddBlogPage> {
         child: ListView(
           physics: BouncingScrollPhysics(),
           children: [
-            Stack(alignment: Alignment.bottomCenter, children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image(
-                  width: MediaQuery.of(context).size.width / 1.1,
-                  height: MediaQuery.of(context).size.height / 3.0,
-                  image: NetworkImage(
-                      'https://randomuser.me/api/portraits/men/73.jpg'),
-                  fit: BoxFit.cover,
-                  // filterQuality: FilterQuality.medium,
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 45,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [Colors.transparent, Colors.black87],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter)),
-              ),
-            ]),
+            _image != null
+                ? Stack(alignment: Alignment.bottomCenter, children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image(
+                        width: MediaQuery.of(context).size.width / 1.1,
+                        height: MediaQuery.of(context).size.height / 3.0,
+                        image: FileImage(_image),
+                        fit: BoxFit.cover,
+                        // filterQuality: FilterQuality.medium,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.transparent, Colors.black87],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter)),
+                    ),
+                  ])
+                : Container(width: 0, height: 0),
             SizedBox(height: 30),
             Container(
               height: 45,
               child: TextFormField(
+                controller: _titleController,
                 style: GoogleFonts.nunito(color: Colors.red, fontSize: 16.0),
                 decoration: InputDecoration(
                   labelStyle:
@@ -92,6 +108,7 @@ class _AddBlogState extends State<AddBlogPage> {
             Container(
               height: 45,
               child: TextFormField(
+                controller: _descriptionController,
                 style: GoogleFonts.nunito(color: Colors.red, fontSize: 16.0),
                 decoration: InputDecoration(
                   labelStyle:
@@ -106,6 +123,25 @@ class _AddBlogState extends State<AddBlogPage> {
                 ),
               ),
             ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              child: Text('Postar', style: GoogleFonts.roboto()),
+              style: TextButton.styleFrom(
+                  primary: Colors.teal,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  backgroundColor: Colors.red),
+            ),
+            SizedBox(height: 30),
+            !_isLoading
+                ? Container(width: 0.0, height: 0.0)
+                : Container(
+                    margin: EdgeInsets.symmetric(horizontal: 60),
+                    child: LinearProgressIndicator(
+                      backgroundColor: Colors.pink,
+                    ),
+                  )
           ],
         ),
       ),
